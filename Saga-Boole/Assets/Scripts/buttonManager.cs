8c9a70be652ab29a3bf8f336fc61cb519b1a1c06
Matheus <matheus_lopes01@hotmail.com>
2019-10-17 12:13:06 -0300

    Menu inicial
    menu inicial com botões funcionando

---
+++ Saga-Boole/Assets/Scripts/buttonManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonManager : MonoBehaviour
+{
+    private bool isActive;
+
+    void start()
+    {
+        isActive = false;
+    }
+    public void carregaCena(string cena)
+    {
+        SceneManager.LoadScene(cena);
+    }
+
+    public void sair()
+    {
+        Application.Quit();
+    }
+
+    public void canvasOnOff(GameObject canvas)
+    {
+        if (isActive)
+        {
+            canvas.SetActive(false);
+            isActive = false;
+        }
+        else
+        {
+            canvas.SetActive(true);
+            isActive = true;
+        }
+    }
+}
